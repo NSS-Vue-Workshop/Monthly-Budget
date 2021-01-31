b8c17156bf690fc291470dccce06238dfaa5dd0f
Adam Sheaffer <adam.e.sheaffer@gmail.com>
2021-01-31 15:48:17 -0600

    remove unused computed prop

--- src/components/PieChart.vue
+++ src/components/PieChart.vue
@@ -9,14 +9,6 @@
 export default {
   props: ["expenses", "totalAmount"],
   computed: {
-    percentageReport() {
-      return this.expenses.map(e => {
-        return {
-          name: e.name,
-          percentage: ((e.amount / this.totalAmount) * 100).toFixed(2)
-        };
-      });
-    },
     series() {
       return this.expenses.map(e => e.amount);
     },
